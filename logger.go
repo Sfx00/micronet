43e1df01a1bf4f8cb504e535f2c3e58a136a659f
SFX <oussamaba348@gmail.com>
2025-10-24 21:28:33 +0100

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,15 +1,38 @@
-package main
+papackage main
 
-// Logger implementation will be added step-by-step.
-import ("log"
-		"os"
+import (
+	"log"
+	"os"
 )
+
 type Logger struct{
 	infoLogger *log.Logger
 	errorLogger *log.Logger
 	verbose bool
 }
 
+
 func NewLogger(verbose bool) *Logger{
-	
+	var logger Logger
+
+	logger.infoLogger = log.New(os.Stdout, "INFO: ",log.LstdFlags)
+	logger.errorLogger = log.New(os.Stderr, "ERROR: ", log.LstdFlags)
+	logger.verbose = verbose
+	return  &logger
+}
+
+func (l *Logger) Info(msg string){
+	l.infoLogger.Println(msg)
+}
+
+func (l *Logger) Error(err error){
+	l.errorLogger.Println(err)
+}
+
+func (l *Logger) Verbose(msg string){
+	if(!l.verbose){
+		return
+	}else{
+		l.infoLogger.Println(msg)
+	}
 }
